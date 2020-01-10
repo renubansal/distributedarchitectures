195e7b719199c93614594825dbbfe6fc95ddd4e1
renu.bansal <renu.bansal@thoughtworks.com>
2020-01-10 14:51:01 +0530

    kafka api to create leader and follower replicas

---
+++ src/main/scala/org/dist/experiment/kafka/RenuSimpleKafkaApi.scala
@@ -0,0 +1,28 @@
+package org.dist.experiment.kafka
+
+import org.dist.kvstore.JsonSerDes
+import org.dist.queue.api.RequestOrResponse
+import org.dist.queue.server.Config
+import org.dist.simplekafka.{LeaderAndReplicaRequest, ReplicaManager}
+
+class RenuSimpleKafkaApi(config: Config, replicaManager: ReplicaManager) {
+
+  def handle(request: RequestOrResponse): RequestOrResponse ={
+    request.requestId match {
+      case 101 =>
+        val leaderAndReplicaRequest = JsonSerDes.deserialize(request.messageBodyJson.getBytes(), classOf[LeaderAndReplicaRequest])
+        leaderAndReplicaRequest.leaderReplicas.foreach(leaderReplica => {
+          val leader = leaderReplica.partitionStateInfo.leader
+          val topicPartition = leaderReplica.topicPartition
+          if (leader.id == config.brokerId){
+            replicaManager.makeLeader(topicPartition)
+          }
+          else{
+            replicaManager.makeFollower(topicPartition,leader.id)
+          }
+        })
+    }
+    RequestOrResponse(101,"",request.correlationId)
+  }
+
+}
---
+++ src/test/scala/org/dist/experiment/kafka/RenuSimpleKafkaApiTest.scala
@@ -0,0 +1,36 @@
+package org.dist.experiment.kafka
+
+import org.dist.kvstore.JsonSerDes
+import org.dist.queue.api.RequestOrResponse
+import org.dist.queue.common.TopicAndPartition
+import org.dist.queue.server.Config
+import org.dist.queue.utils.ZkUtils.Broker
+import org.dist.queue.{TestUtils, ZookeeperTestHarness}
+import org.dist.simplekafka.{LeaderAndReplicaRequest, LeaderAndReplicas, PartitionInfo, ReplicaManager}
+import org.dist.util.Networks
+
+class RenuSimpleKafkaApiTest extends ZookeeperTestHarness {
+
+  test("create leader and follower replicas") {
+    val config = Config(1, new Networks().hostname(), TestUtils.choosePort(), "", List(TestUtils.tempDir().getAbsolutePath))
+    val replicaManager = new ReplicaManager(config)
+    val api = new RenuSimpleKafkaApi(config,replicaManager)
+    val broker1 = new Broker(1, "10.0.0.1", 8080)
+    val broker2 = new Broker(2, "10.0.0.2", 8081)
+    val partitionInfo = PartitionInfo(broker1, List(broker2))
+    val otherPartitionInfo = PartitionInfo(broker2, List(broker1))
+    val leaderAndReplicas = List(
+      LeaderAndReplicas(TopicAndPartition("topic1", 1), partitionInfo),
+      LeaderAndReplicas(TopicAndPartition("topic2", 2), otherPartitionInfo)
+    )
+    val leaderReplicaRequest = LeaderAndReplicaRequest(leaderAndReplicas)
+    val correlationId = 12
+
+    val response = api.handle(RequestOrResponse(101, JsonSerDes.serialize(leaderReplicaRequest),correlationId))
+
+    assert(response.correlationId == correlationId)
+    assert(replicaManager.allPartitions.size == 2)
+
+  }
+
+}
